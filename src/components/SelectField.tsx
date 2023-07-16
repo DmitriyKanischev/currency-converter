cedc22c3e5ad7a061779bdcddb6a1a5669b741e9
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-07-16 11:15:29 +0400

    SelectField added

---
+++ src/components/SelectField.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { IValute } from "../Models";
+
+interface SelectProps {
+    defaultOption: string,
+    options: string[],
+    name: string,
+    value: string[]
+}
+
+const SelectField = ({defaultOption, options, name, value}: SelectProps) => {
+    const [data, setData] = useState({option: ""})
+
+    const handleChange = ({target}: any) => {
+        setData(target.value)
+        console.log(target.value)
+    }
+
+    return ( 
+        <>
+            <select
+                className="w-20 h-10 bg-slate-100 py-[8px] cursor-pointer border rounded text-center"
+                id={name}
+                name={name}
+                defaultValue={value}
+                onChange={handleChange}
+            >
+                <option disabled value="">{defaultOption}</option>
+                {options && options.map(option => 
+                    <option
+                    value={option}
+                    key={option}
+                    selected={option === data.option}
+                    >
+                        {option}
+                    </option>
+                    )}
+
+            </select>
+        </>
+     );
+}
+ 
+export default SelectField; 
--- src/pages/ConvertPage.tsx
+++ src/pages/ConvertPage.tsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
 import NavBar from "../components/NavBar";
+import SelectField from "../components/SelectField";
+import { useCurrency } from "../hooks/CurrencyHook";
 
 const ConvertPage = () => {
+    const {valute} = useCurrency()
+    const [num, setNum] = useState("1")
+
+    const handleChange = ({target}: any) => {
+        setNum(target.value)
+    }
+
+    const test = () => {
+        return +num * 50;
+    }
+
+    const charCodesArr: string[] = []
+    valute?.map(obj => {
+        charCodesArr.push(obj.CharCode)
+    })
+
     return ( 
         <>
             <NavBar/>
@@ -8,22 +27,46 @@ const ConvertPage = () => {
                 <div className="container mx-auto max-w-6xl flex pt-5">
                     <div className="flex flex-col w-1/3 ml-auto mr-28" >
                         <div className="flex mb-2">
-                            <div className="w-20 h-10 mr-1 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">RUR</div>
+                            {/* <div className="w-20 h-10 mr-1 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">RUR</div>
                             <div className="w-20 h-10 mr-1 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">USD</div>
-                            <div className="w-20 h-10 mr-1 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">EUR</div>
-                            <div className="w-20 h-10 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">...</div>
+                            <div className="w-20 h-10 mr-1 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">EUR</div> */}
+
+
+                            {/* <div className="w-20 h-10 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">...</div> */}
+                            <SelectField
+                                defaultOption="..."
+                                options={charCodesArr}
+                                name='valute'
+                                // onChange={handleChange}
+                                value={charCodesArr}
+                        />
+
                         </div>
-                        <input className="border py-2 px-4 rounded flex flex-col text-4xl h-48  items-center justify-between mb-4 bg-slate-100" />
+                        <input 
+                            type="number" 
+                            className="border py-2 px-4 rounded flex flex-col text-4xl h-48  items-center justify-between mb-4 bg-slate-100"
+                            value={num}
+                            onChange={handleChange}
+                            />
                     </div>
 
                     <div className="flex flex-col w-1/3 mr-auto">
                         <div className="flex mb-2">
-                            <div className="w-20 h-10 mr-1 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">RUR</div>
+                            {/* <div className="w-20 h-10 mr-1 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">RUR</div>
                             <div className="w-20 h-10 mr-1 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">USD</div>
-                            <div className="w-20 h-10 mr-1 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">EUR</div>
-                            <div className="w-20 h-10 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">...</div>
+                            <div className="w-20 h-10 mr-1 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">EUR</div> */}
+                            {/* <div className="w-20 h-10 bg-slate-100 py-[8px] cursor-pointer border rounded text-center">...</div> */}
+                            <SelectField
+                                defaultOption="..."
+                                options={charCodesArr}
+                                name='valute'
+                                value={charCodesArr}
+                            />
                         </div>
-                        <input className="border py-2 px-4 rounded flex flex-col text-4xl h-48 items-center justify-between mb-4 bg-slate-100" />
+                        <input 
+                            type="number" 
+                            className="border py-2 px-4 rounded flex flex-col text-4xl h-48 items-center justify-between mb-4 bg-slate-100"
+                            value={test()} />
                     </div>
                 </div>
             </div>
